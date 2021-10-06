61f7ca2d9cd3b2a18ba9c36a6fb712e63222fddd
Alberth henrique <60651996+D7alth@users.noreply.github.com>
2021-10-06 18:27:16 -0300

    creating GET-type routes embedded in the database

--- src/routes/routes-client.ts
+++ src/routes/routes-client.ts
@@ -1,13 +1,16 @@
 import { Router, Request, Response, NextFunction } from 'express'; 
+import UserRepo from '../repo/repo-user'
 const userRoutes = Router();
 
-userRoutes.get('/user',(req:Request, res:Response, next:NextFunction)=>{
-    const users = [{name : 'John', email :'john@zipmail.com'}]; 
+
+userRoutes.get('/user',async (req:Request, res:Response, next:NextFunction)=>{
+    const users = await UserRepo.findAllUsers(); 
     res.json(users).status(200);
 })
-userRoutes.get('/user/:uuid',(req:Request<{uuid : string}>, res:Response, next:NextFunction)=>{
+userRoutes.get('/user/:uuid',async (req:Request<{uuid : string}>, res:Response, next:NextFunction)=>{
     const uuid = req.params.uuid;
-    res.send({uuid}).status(200);
+    const user = await UserRepo.findById(uuid);
+    res.send(user).status(200);
 
 })
 userRoutes.post('/user', (req:Request, res:Response, next:NextFunction)=>{
